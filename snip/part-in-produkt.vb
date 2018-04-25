7dc364f0fdfa6272bb0682c7d0ef1fff72ac1833
ET <et3d@users.noreply.github.com>
2018-04-25 16:41:04 +0200

    Update part-in-produkt.vb

--- snip/part-in-produkt.vb
+++ snip/part-in-produkt.vb
@@ -9,13 +9,14 @@ Set product1 = productDocument1.Product
 Dim products1 As Products
 Set products1 = product1.Products
 
+Dim product2 As Product
+Set product2 = products1.AddNewComponent("Part", "y005" + Str(Math.Rnd(100))
+                                                
 'Dim partname As String
 'Set partname = "y005" + Str(vbmath.Randomize(100))
 
-Dim product2 As Product
-Set product2 = products1.AddNewComponent("Part", "y005" + Str(Math.Rnd(100))
-
         'KNOWN UNRESOLVED ERROR ADDNEWCOMPONENT
+                                                'RESOLVED EXISTING NAME
 'Dim product2 As Product
 'On Error Resume Next
 'Set product2 = products1.AddNewComponent("Part", "y002")
