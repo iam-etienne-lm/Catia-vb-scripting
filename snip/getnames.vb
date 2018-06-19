87247c28ea6fcd13ee19d9c4878c9f0dc3051e3b
ET <et3d@users.noreply.github.com>
2018-06-19 14:31:14 +0200

    Update getnames.vb

--- snip/getnames.vb
+++ snip/getnames.vb
@@ -1,6 +1,7 @@
 Option Explicit
 Sub getnames55()
-'v001
+On Error Resume Next
+'v010 list names and changes the 10th level1 one
 
 Dim specsAndGeomWindow1 As SpecsAndGeomWindow
 Set specsAndGeomWindow1 = CATIA.ActiveWindow
@@ -17,6 +18,32 @@ Set products1 = product1.Products
 
 Debug.Print products1.Count
 Debug.Print CATIA.ActiveDocument.Product.Products.Count
-Debug.Print CATIA.ActiveDocument.Count
+
+Dim intPartcount As Integer
+intPartcount = CATIA.ActiveDocument.Product.Products.Count
+'intPartcount = 10
+
+Dim strNewname As String
+strNewname = "changedname1" & intPartcount
+
+Dim i As Integer
+For i = 1 To intPartcount
+    Debug.Print CATIA.ActiveDocument.Product.Products.Item(i).Name
+    If i = 10 Then
+        CATIA.ActiveDocument.Product.Products.Item(i).Name = strNewname
+    End If
+    
+Next i
+
+
+
+
+
+'Debug.Print CATIA.ActiveDocument.Product.Products.Item(1).Count
+
+'Dim product8 As Product
+'Set product8 = CATIA.ActiveDocument.Product.Products.AddNewComponent("Part", "part00108")
+
+
 
 End Sub
