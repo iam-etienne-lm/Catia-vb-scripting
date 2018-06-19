5952239cbd85be481dcc4d4918508ca77781d5ff
ET <et3d@users.noreply.github.com>
2018-06-19 16:00:35 +0200

    Update create_scratch-product.vb
    adding previous version for understanding

--- snip/create_scratch-product.vb
+++ snip/create_scratch-product.vb
@@ -1,4 +1,4 @@
-Sub create-ScratchProduct()
+Sub CATMain()
 
 Dim oProductDoc As Document
 Set oProductDoc = CATIA.Documents.Add("Product")
@@ -21,4 +21,32 @@ Set products1 = product1.Products
 Dim product2 As Product
 Set product2 = products1.AddNewComponent("Part", "part00103")
 
+'previous version
+'Sub CATMain()
+'
+'Dim documents1 As Documents
+'Set documents1 = CATIA.Documents
+'
+'Dim productDocument1 As ProductDocument
+'Set productDocument1 = documents1.Add("Product")
+'
+'
+'Dim product1 As Product
+'Set product1 = productDocument1.Product
+'
+'Dim products1 As Products
+'Set products1 = product1.Products
+'
+'Dim product2 As Product
+'Set product2 = products1.AddNewComponent("Part", "part00101")
+'
+'Dim product3 As Product
+'Set product3 = products1.AddNewComponent("Part", "part00102")
+'
+'Dim product4 As Product
+'Set product4 = products1.AddNewComponent("Part", "part00103")
+'
+'End Sub
+
 End Sub
+
