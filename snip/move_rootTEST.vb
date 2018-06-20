92a03e73f924f2bf840257f43a8ba000bb028f89
ET <et3d@users.noreply.github.com>
2018-06-20 17:41:24 +0200

    Update move_rootTEST.vb

--- snip/move_rootTEST.vb
+++ snip/move_rootTEST.vb
@@ -1,8 +1,9 @@
 Sub catmain()
-Dim prod1 As ProductDocument
-Set prod1 = CATIA.ActiveDocument
-Set prodInd = prod1.Product
-
+Dim proddoc1 As ProductDocument
+Set proddoc1 = CATIA.ActiveDocument
+Set prod1 = proddoc1.Product 'Subtility resides here. catia.activedomument can't be moved while catia.activedocument.product can be!
+      'indeed a document is not the same as a product.
+      
 Dim moveArray(11)
 moveArray(0) = 1
 moveArray(1) = 0
@@ -13,9 +14,9 @@ moveArray(5) = 0
 moveArray(6) = 0
 moveArray(7) = 0
 moveArray(8) = 1
-moveArray(9) = -20 'translates component 1000 x direction of parent's coordinate space
+moveArray(9) = -20 
 moveArray(10) = 0
 moveArray(11) = 0
-prodInd.Move.Apply moveArray
+prod1.Move.Apply moveArray
 
 End Sub
