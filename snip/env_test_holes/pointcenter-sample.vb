fd52a5c7925f0bb266b0a19dcc4a4c550e89f16f
ET <et3d@users.noreply.github.com>
2018-06-26 10:43:24 +0200

    Update pointcenter-sample.vb

--- snip/env_test_holes/pointcenter-sample.vb
+++ snip/env_test_holes/pointcenter-sample.vb
@@ -1,5 +1,9 @@
-Sub CATMain()
+Option Explicit
 
+Sub CATMain()
+  'no error but not working
+  
+  
 Dim productDocument1 As ProductDocument
 Set productDocument1 = CATIA.ActiveDocument
 Dim documents1 As Documents
@@ -7,19 +11,28 @@ Set documents1 = CATIA.Documents
 Dim partDocument1 As PartDocument
 Set partDocument1 = documents1.Item(2)
 Dim part1 As Part
-Set oPart = partDocument1.Part
+Dim opart As Part
+Set opart = partDocument1.Part
 Dim reference1 As Reference
-Set oHSF = oPart.HybridShapeFactory
+Dim oHsf As HybridShapeFactory
+Set oHsf = opart.HybridShapeFactory
 Dim InputObject(0)
+Dim oCentre 'As Selection
+Dim status
+Dim oTemp 'As Item
+Dim oRef As Reference
+Dim oHb
+Dim oPoint
 
 InputObject(0) = "Edge"
 Set oCentre = CATIA.ActiveDocument.Selection
-Status = oCentre.SelectElement2(InputObject, "Select Circle", False)
+status = oCentre.SelectElement2(InputObject, "Select Circle", False)
 Set oTemp = oCentre.Item(1)
 Set oRef = oTemp.Reference
-Set oPoint = oHSF.AddNewPointCenter(oRef)
-Set oHB = oPart.HybridBodies.Add()
+Set oPoint = oHsf.AddNewPointCenter(oRef)
+Set oHb = opart.HybridBodies.Add()
 oPoint.Compute
-oHB.AppendHybridShape oPoint
+oHb.AppendHybridShape oPoint
 
 End Sub
+
